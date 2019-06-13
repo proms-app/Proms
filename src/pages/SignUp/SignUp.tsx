8c8c1a02c03bb1d4d2d942533845b4c5826138b5
Amaury Wyart <amauryw@bam.tech>
2019-06-13 23:42:17 +0200

    add splashscreen

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,21 +1,25 @@
 // @flow
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
-import { LabelInput } from '../../components/LabelInput/LabelInput.js';
+import { View, TouchableOpacity } from 'react-native';
+import { LabelInput } from '../../components/LabelInput/LabelInput';
+
 type PropsType = {
   navigation: NavigationType;
 };
 
 class SignUp extends React.Component<PropsType> {
   goBack = () => {
-    this.props.navigation.goBack();
+    this.props.navigation.navigate('auth');
   };
 
   render() {
     return (
       <View style={{ flex: 1 }}>
         <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ width: 30, height: 30, flex: 1, backgroundColor: 'red' }} />
+          <TouchableOpacity
+            onPress={this.goBack}
+            style={{ width: 30, height: 30, flex: 1, backgroundColor: 'red' }}
+          />
         </View>
         <LabelInput title="Nom" />
         <LabelInput title={'Prenom'} />
