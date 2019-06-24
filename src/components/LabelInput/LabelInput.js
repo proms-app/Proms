1c6d12906366dd0b2f3fa61132737f1b919f2ef8
pbmaddens <pierre-baptiste.maddens@gadz.org>
2019-06-24 20:53:38 +0200

    labelinput

--- src/components/LabelInput/LabelInput.js
+++ src/components/LabelInput/LabelInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, TextInput } from 'react-native';
 
 export const LabelInput = props => (
-  <View style={{padding: 10, justifyContent: 'center'}}>
+  <View style={{padding: 10}}>
     <TextInput style={{height: 40}}
     placeholder={props.title} />
   </View>
