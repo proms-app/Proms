38097d69f79706628fbb85a7dbcd460fe375dca9
Amaury Wyart <amauryw@bam.tech>
2019-08-27 09:27:28 +0200

    tmp

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
-  extends: 'bambi',
+  root: true,
+  extends: ["bambi", "@react-native-community"],
   rules: {
-    'prettier/prettier': 'error',
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    'no-undef': 'off',
-    'promise/avoid-new': 'off',
-  },
+    "prettier/prettier": "error",
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-undef": "off",
+    "promise/avoid-new": "off"
+  }
 };
+module.exports = {};
